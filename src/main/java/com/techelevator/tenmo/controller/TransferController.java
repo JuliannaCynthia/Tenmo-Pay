713686f1d02aab68b8ce0ab08d88aefcfc1e2bd2
Seth Aker <saker132@gmail.com>
2023-08-23 15:04:56 -0500

    Finished TransferController

--- src/main/java/com/techelevator/tenmo/controller/TransferController.java
+++ src/main/java/com/techelevator/tenmo/controller/TransferController.java
@@ -3,6 +3,7 @@ package com.techelevator.tenmo.controller;
 import com.techelevator.tenmo.dao.TransferDao;
 import com.techelevator.tenmo.dao.UserDao;
 import com.techelevator.tenmo.model.Transfer;
+import com.techelevator.tenmo.model.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -36,10 +37,10 @@ public class TransferController {
 
 
     @RequestMapping(value = "/history", method = RequestMethod.GET)
-    public List<Transfer> transferHistory(@RequestParam String username,
+    public List<Transfer> transferHistory(@Valid @RequestBody User user,
                                           @RequestParam (required = false) String friendUsername){
 
-        int userId = userDao.findIdByUsername(username);
+        int userId = user.getId();
         Integer friendUserId = userDao.findIdByUsername(friendUsername);
 
         List<Transfer> transferList = transferDao.viewTransferFromHistory(userId, friendUserId);
@@ -51,5 +52,10 @@ public class TransferController {
     @RequestMapping(value = "/history/{transferId}", method = RequestMethod.GET)
     public Transfer getTransferById(@PathVariable Integer transferId){
         return transferDao.getTransferById(transferId);
+    }
+
+    @RequestMapping(value = "/pending", method = RequestMethod.GET)
+    public List<Transfer> viewPendingTransfers(@Valid @RequestBody User user){
+        return transferDao.viewPendingTransfers(user.getId());
     }
 }
