e2299a3c5566b061294a35390ac0a98b393e00f1
Julianna Irons[B[A[B[A[B <ironsjulianna@gmail.com>
2023-08-25 16:28:24 -0400

    initial graphics and baby menu prompts for cli

---
+++ client/src/main/java/com/techelevator/services/GraphicServices.java
@@ -0,0 +1,26 @@
+package com.techelevator.services;
+
+public class GraphicServices {
+
+
+    public void openingGraphic(){
+        System.out.println("########++++++++++++++++++++++++++++########");
+        System.out.println("####                                    ####");
+        System.out.println("###             WELCOME  TO              ###");
+        System.out.println("###             TENMO - PAY              ###");
+        System.out.println("####                                    ####");
+        System.out.println("########++++++++++++++++++++++++++++########");
+        System.out.println("~Where you are 'ten'-tamount to our success!~\n\n");
+    }
+
+
+    public void logRegMenu(){
+        System.out.println("########++++++++++++++++++++++++++++########");
+        System.out.println("####                                    ####");
+        System.out.println("###           #1  {~Log In~}             ###");
+        System.out.println("###           #2 {~Register~}            ###");
+        System.out.println("####                                    ####");
+        System.out.println("########++++++++++++++++++++++++++++########\n\n");
+    }
+
+}
